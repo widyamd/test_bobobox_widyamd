e327e5bdabf3e6037f377a99cbbf72f66175a0c0
widya mustika <widyamustikad@gmail.com>
2024-10-17 18:26:49 +0700

    grid layout

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+function Layout() {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Layout;
